aa458bf4b3a98670a88136092c4bb0c7bcf29837
Jonathan Guthrie <jono@perfect.org>
2017-10-11 16:10:35 -0400

    update for perfect 3, swift 4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-08 17:28:01 +0000
+// Date: 2017-10-11 20:05:49 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectTurnstilePostgreSQL",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
-        .Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", majorVersion: 2),
-    ]
+	name: "PerfectTurnstilePostgreSQL",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/SwiftORM/Postgres-StORM.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
+		.Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
+		.Package(url: "https://github.com/PerfectSideRepos/Turnstile-Perfect.git", majorVersion: 3),
+	]
 )
